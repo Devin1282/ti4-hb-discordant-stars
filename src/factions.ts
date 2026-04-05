fbb1815b227b603d602d5ee5b92569cc8d0d676a
Darrell <darrell.anderson@gmail.com>
2026-04-05 14:01:09 -0700

    init

---
+++ src/factions.ts
@@ -0,0 +1,4 @@
+import { FactionSchemaType } from "ti4-ttpg-ts";
+
+export const factions: Array<FactionSchemaType> = []
+
---
+++ src/generate.test.ts
@@ -0,0 +1,6 @@
+import { generate } from "ti4-hb-helper";
+import { homebrew } from "./homebrew";
+
+it("generate", async () => {
+  await generate(homebrew);
+})
