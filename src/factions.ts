d30b45a1f74663a6cb1e72de0313f1e1c2b03dc1
Devin1282 <dl.johnson.1995@gmail.com>
2026-08-28 14:35:30 -0500

    Added Leaders and Promissory prebuilds

--- src/factions.ts
+++ src/factions.ts
@@ -1,4 +1,35 @@
 import { FactionSchemaType } from "ti4-ttpg-ts";
 
-export const factions: Array<FactionSchemaType> = []
+export const factions: Array<FactionSchemaType> = [
+    {
+      nsidName: "florzen-profiteers",
+      name: "Florzen Profiteers",
+      abbr: "Florzen",
+      abilities: ["mercenaries","data-leak","shadow-markets"],
+      breakthroughs: [
+        {
+          breakthrough: "reverie-implants",
+          techEquivalence: ["blue", "green"],
+        },
+      ],
+      commodities: 4,
+      home: 1002,
+      leaders: {
+        agents: ["sal-gavda"],
+        commanders: ["quaxdol-junitas"],
+        heroes: ["banua-gowen"],
+        mechs: ["privateer"],
+      },
+      promissories: ["underground-market"],
+      startingTechs: ["scanlink-drone-network","neural-motivator"],
+      startingUnits: {
+        carrier: 2,
+        fighter: 4,
+        infantry: 4,
+        spaceDock: 1,
+      },
+      factionTechs: ["blackmail-programs"],
+      unitOverrides: ["man-o-war", "corsair-1", "corsair-2"],
+    }
+];
 
